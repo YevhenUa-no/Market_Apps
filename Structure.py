0f83095763c60c71eb867e675accdae54dc51c69
YevhenUa-no <127596731+YevhenUa-no@users.noreply.github.com>
2025-04-05 00:17:52 +0200

    Update Structure.py

--- Structure.py
+++ Structure.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("makret_tracking.py", name="Market Tracking"),
-    st.Page("your_new_page.py", name="Your New Page"),  # Show me this line
+     # Show me this line
 ])
