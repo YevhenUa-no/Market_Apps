f50d27b26155705757aa1bca19f85b41c188d5c2
YevhenUa-no <127596731+YevhenUa-no@users.noreply.github.com>
2025-04-05 00:23:46 +0200

    Update Structure.py

--- Structure.py
+++ Structure.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-create_page = st.Page("create.py", title="Create entry", icon=":material/add_circle:")
-delete_page = st.Page("delete.py", title="Delete entry", icon=":material/delete:")
+create_page = st.Page("market_tracking.py", title="Create entry", icon=":material/add_circle:")
+# delete_page = st.Page("delete.py", title="Delete entry", icon=":material/delete:")
 
 pg = st.navigation([create_page, delete_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
