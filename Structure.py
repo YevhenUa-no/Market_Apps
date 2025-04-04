b0b9293f8273280b84e8217d7c8a6f8132c45afb
YevhenUa-no <127596731+YevhenUa-no@users.noreply.github.com>
2025-04-05 00:28:26 +0200

    Update Structure.py

--- Structure.py
+++ Structure.py
@@ -3,6 +3,6 @@ import streamlit as st
 create_page = st.Page("makret_tracking.py", title="Create entry", icon=":material/add_circle:")
 # delete_page = st.Page("delete.py", title="Delete entry", icon=":material/delete:")
 
-pg = st.navigation([create_page, delete_page])
+pg = st.navigation([create_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
